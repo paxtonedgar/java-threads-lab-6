ba99131c66464df1e67b2175733086e87a7ee2c7
Paxton Edgar <paxtonedgar3@gmail.com>
2022-07-27 12:18:25 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
+            PrimeLogger prime = new PrimeLogger(num);
+            executor.execute(prime);
             // submit tasks to your executor
         }
     }
@@ -21,8 +25,11 @@ class PrimeLogger implements Runnable {
         this.num = num;
     }
 
+    public void isPrime(int num){
+        IntStream.rangeClosed(2, num/2).filter(i -> num%i!=0).findAny().ifPresentOrElse((i)-> System.out.println(num), () -> System.out.println("Not Prime"));
+    }
     @Override
     public void run() {
-        // print num if it is prime
+        isPrime(num);
     }
 }
